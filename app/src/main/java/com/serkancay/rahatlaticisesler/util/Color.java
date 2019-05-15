fb5193dc798fffcaac4b0849ffab98d9b4924edd
Serkan Çay <sserkancay@gmail.com>
2019-05-16 00:52:43 +0300

    Random renk uretici util eklendi

---
+++ app/src/main/java/com/serkancay/rahatlaticisesler/util/Color.java
@@ -0,0 +1,18 @@
+package com.serkancay.rahatlaticisesler.util;
+
+import java.util.Random;
+
+/**
+ * Created by S.Serkan Cay on 16.05.2019
+ */
+
+public class Color {
+
+    public static int generateRandomColor() {
+        Random random = new Random();
+        int color = android.graphics.Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        return color;
+    }
+
+}
+
