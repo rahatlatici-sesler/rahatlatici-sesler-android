86b3121264e3b44021a4e03c98fac239619d5562
Serkan Çay <sserkancay@gmail.com>
2019-05-16 03:35:10 +0300

    ApiEndpoint eklendi

---
+++ app/src/main/java/com/serkancay/rahatlaticisesler/data/network/ApiEndpoint.java
@@ -0,0 +1,16 @@
+package com.serkancay.rahatlaticisesler.data.network;
+
+import com.serkancay.rahatlaticisesler.BuildConfig;
+
+/**
+ * Created by S.Serkan Cay on 16.05.2019
+ */
+
+public final class ApiEndpoint {
+
+    public static final String ENDPOINT_FAVORITE_LIST = BuildConfig.BASE_URL + "/favorite_list.html";
+
+    private ApiEndpoint() {
+    }
+
+}
